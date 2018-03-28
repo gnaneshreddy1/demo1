ed814d1404adcd65078f0ef0392ae7635dda0b8c
gnaneshreddy1 <gnaneshsaireddy@gmail.com>
2018-03-28 14:53:20 -0500

    updated javafile

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
-class 1
+class hello
 {
-public static void main()
+public static void main(String[] args)
 {       
 
-   System.out.priintln("Hello World");
+   System.out.println("Hello World");
 }
 }
